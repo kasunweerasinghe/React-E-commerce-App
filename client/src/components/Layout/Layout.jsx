2af3ced7ad6e9a96cdd199ad11eff9f35a4ff4de
kasunweerasinghe <kasunrachintha2018@gmail.com>
2023-07-15 18:47:04 +0530

    add user register form and styles

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import Footer from "./Footer";
-import Header from "./Header";
+import React from 'react';
+import Footer from './Footer';
+import Header from './Header';
 
 const Layout = ({ children }) => {
   return (
     <div>
       <Header />
-      <main style={{ minHeight: "73vh" }}>{children}</main>
+      <main style={{ minHeight: '73vh' }}>{children}</main>
       <Footer />
     </div>
   );
