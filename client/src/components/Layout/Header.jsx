c202d9b89c9f06f0a70d4f3df74734f851901037
kasunweerasinghe <kasunrachintha2018@gmail.com>
2023-07-16 10:32:39 +0530

    add user profile ui

--- client/src/components/Layout/Header.jsx
+++ client/src/components/Layout/Header.jsx
@@ -31,7 +31,7 @@ const Header = () => {
           </button>
           <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
             <Link to="/" className="navbar-brand">
-              🛒 E-Commerce App
+              🛒 Ecommerce App
             </Link>
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <li className="nav-item">
@@ -71,7 +71,12 @@ const Header = () => {
                     </NavLink>
                     <ul className="dropdown-menu">
                       <li>
-                        <NavLink to="/dashboard" className="dropdown-item">
+                        <NavLink
+                          to={`/dashboard/${
+                            auth?.user?.role === 1 ? 'admin' : 'user'
+                          }`}
+                          className="dropdown-item"
+                        >
                           Dashboard
                         </NavLink>
                       </li>
