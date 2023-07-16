444a5a886fddacccbd10db50c6b2ad15bd8df6e6
kasunweerasinghe <kasunrachintha2018@gmail.com>
2023-07-16 13:51:21 +0530

    add price filter

--- client/src/App.js
+++ client/src/App.js
@@ -35,9 +35,9 @@ function App() {
           <Route path="admin" element={<AdminDashboard />} />
           <Route path="admin/create-category" element={<CreateCategory />} />
           <Route path="admin/create-product" element={<CreateProduct />} />
-          <Route path="admin/users" element={<Users />} />
           <Route path="admin/product/:slug" element={<UpdateProduct />} />
           <Route path="admin/products" element={<Products />} />
+          <Route path="admin/users" element={<Users />} />
         </Route>
 
         <Route path="/register" element={<Register />} />
