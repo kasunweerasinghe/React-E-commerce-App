c69bc25f4dd27d834269e4b3691a85723b2965a9
kasunweerasinghe <kasunrachintha2018@gmail.com>
2023-07-16 10:47:06 +0530

    added categoryController

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ import colors from 'colors';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
 import connectDB from './config/db.js';
-import authRoutes from "./routes/authRoute.js";
-import cors from "cors";
+import authRoutes from './routes/authRoute.js';
+import categoryRoutes from './routes/categoryRoutes.js';
+import cors from 'cors';
 
 // configure env
 dotenv.config();
@@ -21,7 +22,8 @@ app.use(express.json());
 app.use(morgan('dev'));
 
 //routes
-app.use("/api/v1/auth", authRoutes);
+app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/category', categoryRoutes);
 
 // rest api
 app.get('/', (req, res) => {
