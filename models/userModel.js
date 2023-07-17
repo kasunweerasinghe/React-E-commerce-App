85193d63f8f248259ded8c8d4259e1a447bdeefc
kasunweerasinghe <kasunrachintha2018@gmail.com>
2023-07-17 10:43:43 +0530

    update user profile data change

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
@@ -21,11 +21,7 @@ const userSchema = new mongoose.Schema(
       required: true,
     },
     address: {
-      type: String,
-      required: true,
-    },
-    answer: {
-      type: String,
+      type: {},
       required: true,
     },
     answer: {
@@ -40,4 +36,4 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-export default mongoose.model('users', userSchema);
+export default mongoose.model("users", userSchema);
